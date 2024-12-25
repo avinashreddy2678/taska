64b44fa9c7a0921386f8c5c480f68d1a0fb21001
avinash <imavinashreddy777@gmail.com>
2024-12-25 16:00:39 +0530

    changes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,14 +1,20 @@
 import { Router } from "express";
-import { handleLogout, ResendOtp, Signin, Signup, userUpdates, verifyOtp } from "../Controller/authController.js";
+import {
+  handleLogout,
+  ResendOtp,
+  Signin,
+  Signup,
+  userUpdates,
+  verifyOtp,
+} from "../Controller/authController.js";
 
 const router = Router();
 
 router.post("/signup", Signup);
 router.post("/signin", Signin);
-router.post("/verify-otp",verifyOtp);
-router.get("/resend-otp",ResendOtp);
-router.get("/user-updates",userUpdates);
-router.post("/logout",handleLogout);
-
+router.post("/verify-otp", verifyOtp);
+router.get("/resend-otp", ResendOtp);
+router.get("/user-updates", userUpdates);
+router.post("/logout", handleLogout);
 
 export { router as AuthRoute };
