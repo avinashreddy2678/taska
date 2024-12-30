bda788ef44a1962a2ccc27017ecef956bf1852d7
Anjith Reddy <anjithreddy5@gmail.com>
2024-12-30 15:48:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ app.use("/group", VerifyUser, GroupRouter);
 app.use("/fcm", VerifyUser, fcmRouter);
 app.use("/user", VerifyUser, userDetailRouter);
 
+app.use((req, res, next) => {
+  console.log('Incoming request:', req.method, req.url);
+  next();
+});
 
 app.listen(4000,'0.0.0.0', () => {
   console.log("Server is running");
